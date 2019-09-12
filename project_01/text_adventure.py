9d2304887c94e71dbd47f9b170393299a55550e2
Enrique Pesantez <enrique.pesantez1@marist.edu>
2019-09-12 13:15:18 -0400

    Prjoect Number 1

---
+++ project_01/text_adventure.py
@@ -0,0 +1,36 @@
+def title():
+  print("Crazy Nights")
+  print('You live in a small town where every knows each other. However, the old Hag who you live next to was murdered!! It is your job as the towns sheriff to figure out who killed her.')
+title()
+def PlayerLocation(string = print()):
+  print('Location: ', string)
+CrimeScene = 'Crime Scene'
+PoliceStation = 'Police Station'
+PoliceCar = 'Police Car'
+MikeHouse = 'Mikes House'
+MikeBasement = 'Mikes Basement'
+name = input("What is your name:", )
+input('Press Enter to Continue: ')
+score = 0
+print('Your score is:', score)
+input('Press Enter to Continue: ')
+PlayerLocation(CrimeScene)
+score = score + 5
+print('Your score is:', score)
+input('Press Enter to Continue: ')
+PlayerLocation(PoliceStation)
+score = score + 5
+print('Your score is:', score)
+input('Press Enter to Continue: ')
+PlayerLocation(PoliceCar)
+score = score + 5
+print('Your score is:', score)
+input('Press Enter to Continue: ')
+PlayerLocation(MikeHouse)
+score = score + 5
+print('Your score is:', score)
+input('Press Enter to Continue: ')
+PlayerLocation(MikeBasement)
+score = score + 5
+print('Your score is:', score)
+input('Press Enter to Continue: ')
