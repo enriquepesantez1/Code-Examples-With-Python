6f6832c27dff3f74ae4736b1aae24f80ccd6cf09
Enrique Pesantez <enrique.pesantez1@marist.edu>
2019-09-16 12:47:07 -0400

    Second Itteration of the Game

--- project_01/text_adventure.py
+++ project_01/text_adventure.py
@@ -1,36 +1,20 @@
 def title():
   print("Crazy Nights")
   print('You live in a small town where every knows each other. However, the old Hag who you live next to was murdered!! It is your job as the towns sheriff to figure out who killed her.')
-title()
-def PlayerLocation(string = print()):
-  print('Location: ', string)
-CrimeScene = 'Crime Scene'
-PoliceStation = 'Police Station'
-PoliceCar = 'Police Car'
-MikeHouse = 'Mikes House'
-MikeBasement = 'Mikes Basement'
-name = input("What is your name:", )
-input('Press Enter to Continue: ')
+def ending():
+  print("The End")
+  print("Created by Enrique Pesantez")
+  print("Email: enrique.pesantez1")
+player_name = input("What is your name:", )
+player_location = ['Crime Scene', 'Police Station', 'Police Car', 'Mikes House', 'Mikes Basement' ]
+story = ['1','2','3','4','5']
 score = 0
-print('Your score is:', score)
-input('Press Enter to Continue: ')
-PlayerLocation(CrimeScene)
-score = score + 5
-print('Your score is:', score)
-input('Press Enter to Continue: ')
-PlayerLocation(PoliceStation)
-score = score + 5
-print('Your score is:', score)
-input('Press Enter to Continue: ')
-PlayerLocation(PoliceCar)
-score = score + 5
-print('Your score is:', score)
-input('Press Enter to Continue: ')
-PlayerLocation(MikeHouse)
-score = score + 5
-print('Your score is:', score)
-input('Press Enter to Continue: ')
-PlayerLocation(MikeBasement)
-score = score + 5
-print('Your score is:', score)
-input('Press Enter to Continue: ')
+#running the game
+title()
+for i in range(5):
+    print(player_location[i])
+    print(story[i])
+    score = score + 5
+    print("Your score: ", score)
+    input('Press Enter To Continue:', )
+ending()
