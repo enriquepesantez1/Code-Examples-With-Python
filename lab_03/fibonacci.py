1d3e9cccbe1b9d029ae65b8e654c23a4a1909ad9
Enrique Pesantez <enrique.pesantez1>
2019-09-19 14:23:09 -0400

    This is the Fibonacci code

---
+++ lab_03/fibonacci.py
@@ -0,0 +1,22 @@
+# Enrique Peantez Created this Program on 9/19/19
+# This program runs the fibonacci sequence
+
+
+def fibonacci(n): 
+    a = 0
+    b = 1
+    for i in range(n+1):
+        if i < 0: 
+            print("Incorrect input") 
+        elif i == 0: 
+            print(a) 
+        elif i == 1: 
+            print(b) 
+        else: 
+            c = a + b 
+            a = b 
+            b = c 
+            print(b)
+
+
+fibonacci(int(input("What nth number:")))
