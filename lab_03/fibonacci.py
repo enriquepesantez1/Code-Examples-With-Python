523d4a639859475757ecc2335be326bc78e8e7e9
Enrique Pesantez <enrique.pesantez1>
2019-09-19 14:39:10 -0400

    This is my Fibonacci sequence

--- lab_03/fibonacci.py
+++ lab_03/fibonacci.py
@@ -5,10 +5,8 @@
 def fibonacci(n): 
     a = 0
     b = 1
-    for i in range(n+1):
-        if i < 0: 
-            print("Incorrect input") 
-        elif i == 0: 
+    for i in range(0, n+1):
+        if i == 0: 
             print(a) 
         elif i == 1: 
             print(b) 
