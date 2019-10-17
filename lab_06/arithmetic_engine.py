38906977e5a508ec59eca7a323daf345d9b23856
Enrique esantez <enrique.pesantez1@marist.edu>
2019-10-17 19:38:26 -0400

    Final Version of the Engine

--- lab_06/arithmetic_engine.py
+++ lab_06/arithmetic_engine.py
@@ -2,36 +2,51 @@
 # Enrique Pesantez
 # 10-17-2019
 
+# This is the intro for the program
 def show_intro():
     print('Wlecome to the Arithmetic Engine!')
     print('=================================\n')
     print("Valid commands are 'add', 'mult', 'sub', 'div', and 'quit'.\n")
 
+# This displays the outro of the program
 def show_outro():
     print('\nThank you for using the Arithmetic Engine...')
     print('\nPlease come back again soon!')
 
+# Main function of the program
 def do_loop():
-    num1 = int(input("Enter the first number: "))
-    num2 = int(input("Enter the Second Number: "))
-    cmd = input('What computation do you want to perfrorm?: ')
-    cmd.lower()
-    if cmd == 'add':
-        result = num1 + num2
-    elif cmd == 'sub':
-        result = num1 - num2
-    elif cmd == "mult":
-        result = num1 * num2
-    elif cmd == 'div':
-        result = num1 / num2
-    elif cmd == 'quit':
-        return
-    else:
-        print(cmd, "is not a valid command")
-        return
 
-    print('The result is:', result, '\n')
+    while True:
+    # Asks for the input of the user
+        num1 = int(input("Enter the first number: "))
+        num2 = int(input("Enter the Second Number: "))
+        cmd = input('What computation do you want to perfrorm? Or type quit to exit: ')
+        cmd.lower()
+        # Series of if statements that check the user commands
+        if cmd == 'add':
+            result = num1 + num2
+            break
+        elif cmd == 'sub':
+            result = num1 - num2
+            break
+        elif cmd == "mult":
+            result = num1 * num2
+            break
+        elif cmd == 'div':
+            try:
+                result = num1 / num2
+            except:
+                print("Unable to Divide by zero!")
+                continue
+        elif cmd == 'quit':
+            break
+        else:
+            print(cmd, "is not a valid command")
+            continue
+        # Prints the result of the program
+        print('The result is:', result, '\n')
 
+# Main function of the program that runs it
 def main():
     show_intro()
     do_loop()
